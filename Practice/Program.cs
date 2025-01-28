d66d7dfbbc2f1aa6f50d3d9e7a088cea15723875
Sudhanshu-77 <stripathi1721@gmail.com>
2025-01-28 14:37:52 +0530

    Update .gitignore to exclude build artifacts

---
+++ Practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Person
+{
+    // Fields
+    private string name;
+    private int age;
+
+    // Constructor
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display details
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an object of the Person class
+        Person person1 = new Person("Justice", 25);
+        
+        // Call the method to display details
+        person1.DisplayDetails();
+    }
+}
